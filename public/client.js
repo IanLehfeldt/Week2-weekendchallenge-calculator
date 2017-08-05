ef41de8d599853106dea44814595a6c62e93d7e7
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-08-04 20:18:13 -0500

    buttons were added and tested

--- public/client.js
+++ public/client.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('Jquery is being sourced!!');
 
+    $('#addButton').on('click', function () {
+        console.log('Add button was click!');
+    })
 
+    $('#subtractButton').on('click', function () {
+        console.log('Subtract button was click!');
+    })
+    
+    $('#multiplyButton').on('click', function () {
+        console.log('Multiply button was click!');
+    })
+
+    $('#divideButton').on('click', function () {
+        console.log('Divide button was click!');
+    })
 });
